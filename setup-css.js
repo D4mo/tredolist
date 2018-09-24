9dac7f1bff38c13f8580d9745fb13a5c5c1071a7
Olivier Cado <netismic@gmail.com>
2018-09-24 13:05:06 +0200

    Fix error when opening a card window

--- setup-css.js
+++ setup-css.js
@@ -1,72 +1,4 @@
 
-// Utilities
-
-function insertCss(cssFile, id) {
-  if (document.getElementById(id) === null) {
-    var css = document.createElement('link');
-    css.id = id;
-    css.type = 'text/css';
-    css.rel = 'stylesheet';
-    css.href = cssFile;
-    document.getElementsByTagName('head')[0].appendChild(css);
-  }
-}
-
-function findStylesheet(token) {
-  var currentCssArray = document.querySelectorAll('link[rel=stylesheet]');
-  var i;
-  for (i=0; i<currentCssArray.length; ++i) {
-      var styleSh = currentCssArray[i];
-      if (styleSh.getAttribute('href').lastIndexOf(token) !== -1)
-          return styleSh;
-  }
-  return null;
-}
-
-function switchStylesheet(styleSh, newShortFilename) {
-  var href = styleSh.getAttribute('href');
-  var sflnPos = href.lastIndexOf('/') + 1;
-  if (sflnPos == 0)
-      return;
-  href = href.substr(0, sflnPos) + newShortFilename;
-  styleSh.setAttribute('href', href);
-}
-
-function setNewStylesheet(cssToReplace, newCss) {
-  var styleSh = findStylesheet(cssToReplace);
-  if (styleSh !== null)
-    switchStylesheet(styleSh, newCss);
-  else
-    insertCss(chrome.extension.getURL("css/"+newCss));
-}
-
-function catchNodeByClass(className, callback) {
-  var nodes = document.getElementsByClassName(className);
-  for (var node of nodes) {
-    callback(node);
-  }
-}
-
-function getParent(node, selector) {
-  for (var p = node && node.parentElement; p; p = p.parentElement) {
-    if (p.matches(selector))
-      return p;
-  }
-  return null;
-}
-
-function loadSettings(boardId, defaultId=undefined) {
-  chrome.storage.sync.get([boardId], function(result) {
-      var configObj = result[boardId];
-      if (configObj) {
-          document.querySelector('#layout [value='+configObj.layout+'}').checked = true;
-          document.querySelector('#coloring [value='+configObj.coloring+'}').checked = true;
-      }
-      else if (defaultId)
-          loadSettings(defaultId);
-  });
-}
-
 function getBoardId() {
   var matches = window.location.href.match(/^.*:\/\/trello\.com\/b\/(.*)\/.*/);
   if (! (matches && matches.length > 1))
@@ -74,234 +6,305 @@ function getBoardId() {
   return matches[1];
 }
 
-// Main logic
-
-var board = document.getElementById('board');
-var layouts = ['vertical', 'mixed', 'horizcards'];
-
-function setLayout(layoutName) {
-  if (!board.classList)
-    return;
-  board.classList.add('layout-trello-'+layoutName);
-  for (var layout of layouts) {
-    if (layout !== layoutName)
-      board.classList.remove('layout-trello-'+layout);
-  }
-}
-
-insertCss(chrome.extension.getURL('css/layout.css'), 'layoutcss');
-
-var configObj = {
-  layout: '',
-  coloring: ''
-};
-var hasSpecificConfig = false;
 var boardId = getBoardId();
-var defaultSettingKey = '*';
+if (boardId !== null) {
 
-function onSettingChanged(key, newConfigObj) {
-  if (key === boardId) {
-    hasSpecificConfig = true;
-    //console.log("Tredolist: loading page-specific settings for "+key);
-    //console.log(newConfigObj);
-  }
-  else if ((key !== defaultSettingKey) || hasSpecificConfig)
-    return;
-  //else
-  //  console.log("Tredolist: loading default settings");
-  
-  for (var configKey in configObj) {
-    if (newConfigObj[configKey] !== configObj[configKey]) {
-      switch (configKey) {
-        case 'layout':
-          setLayout(newConfigObj[configKey]);
-          break;
-        case 'coloring':
-          setNewStylesheet(
-            'theme-'+configObj[configKey]+'.css',
-            'theme-'+newConfigObj[configKey]+'.css');
-        break;
-      }
-      configObj[configKey] = newConfigObj[configKey];
+  // Utilities
+
+  function insertCss(cssFile, id) {
+    if (document.getElementById(id) === null) {
+      var css = document.createElement('link');
+      css.id = id;
+      css.type = 'text/css';
+      css.rel = 'stylesheet';
+      css.href = cssFile;
+      document.getElementsByTagName('head')[0].appendChild(css);
     }
   }
-}
 
-// Load settings
-chrome.storage.sync.get([boardId], function(result) {
-  if (result[boardId])
-    onSettingChanged(boardId, result[boardId]);
-  else {
-    chrome.storage.sync.get([defaultSettingKey], function(resultD) {
-      if (resultD[defaultSettingKey])
-        onSettingChanged((defaultSettingKey), resultD[defaultSettingKey]);
+  function findStylesheet(token) {
+    var currentCssArray = document.querySelectorAll('link[rel=stylesheet]');
+    var i;
+    for (i=0; i<currentCssArray.length; ++i) {
+        var styleSh = currentCssArray[i];
+        if (styleSh.getAttribute('href').lastIndexOf(token) !== -1)
+            return styleSh;
+    }
+    return null;
+  }
+
+  function switchStylesheet(styleSh, newShortFilename) {
+    var href = styleSh.getAttribute('href');
+    var sflnPos = href.lastIndexOf('/') + 1;
+    if (sflnPos == 0)
+        return;
+    href = href.substr(0, sflnPos) + newShortFilename;
+    styleSh.setAttribute('href', href);
+  }
+
+  function setNewStylesheet(cssToReplace, newCss) {
+    var styleSh = findStylesheet(cssToReplace);
+    if (styleSh !== null)
+      switchStylesheet(styleSh, newCss);
+    else
+      insertCss(chrome.extension.getURL("css/"+newCss));
+  }
+
+  function catchNodeByClass(className, callback) {
+    var nodes = document.getElementsByClassName(className);
+    for (var node of nodes) {
+      callback(node);
+    }
+  }
+
+  function getParent(node, selector) {
+    for (var p = node && node.parentElement; p; p = p.parentElement) {
+      if (p.matches(selector))
+        return p;
+    }
+    return null;
+  }
+
+  function loadSettings(boardId, defaultId=undefined) {
+    chrome.storage.sync.get([boardId], function(result) {
+        var configObj = result[boardId];
+        if (configObj) {
+            document.querySelector('#layout [value='+configObj.layout+'}').checked = true;
+            document.querySelector('#coloring [value='+configObj.coloring+'}').checked = true;
+        }
+        else if (defaultId)
+            loadSettings(defaultId);
     });
   }
-});
-chrome.storage.onChanged.addListener(function(changes, areaName) {
-  for (key in changes) {
-    onSettingChanged(key, changes[key].newValue);
-  }
-});
 
-var States = ['canceled-tdl', 'done-tdl', 'for-later-tdl', 'waiting-for-tdl', 'in-progress-tdl']; // in order of priority
-var Fields = ['Canceled', 'Done', 'For Later', 'Waiting For', 'Started'];
+  // Main logic
 
-function getStateFromBadgeText(node) {
-  if (!node)
-    return undefined;
+  var board = document.getElementById('board');
+  var layouts = ['vertical', 'mixed', 'horizcards'];
 
-  // Using custom fields
-  for (var iState in Fields) {
-    if (node.innerText === Fields[iState])
-      return States[iState];
-  }
-
-  // Using checklist
-  var toks = node.innerText.match(/^(\d+)\/(\d+)$/);
-  if (toks && toks.length > 2) {
-    var numDone = toks[1];
-    var numTodo = toks[2];
-    if (numDone === numTodo)
-      return 'done-tdl';
-    else if (numDone > 0)
-      return 'in-progress-tdl';
-    else
-      return 'not-started-tdl';
-  }
-  return undefined;
-}
-
-function getStateFromTitle(node) {
-  if (node && node.innerText) {
-    if (node.innerText.match(/^\(.*\)$/) !== null)
-      return 'for-later-tdl';
-    if (node.innerText.match(/\<.*\>/) !== null)
-      return 'waiting-for-tdl';
-  }
-  return undefined;
-}
-
-function getMainState(compoundStates) {
-  for (var st of States) {
-    if (compoundStates.indexOf(st) !== -1) {
-      return st;
+  function setLayout(layoutName) {
+    if (!board.classList)
+      return;
+    board.classList.add('layout-trello-'+layoutName);
+    for (var layout of layouts) {
+      if (layout !== layoutName)
+        board.classList.remove('layout-trello-'+layout);
     }
   }
-  return undefined;
-}
 
-function setState(card, state) {
-  if (!card.classList)
-    return;
-  for (var st of States) {
-    card.classList.toggle(st, (st === state));
-  }
-}
+  insertCss(chrome.extension.getURL('css/layout.css'), 'layoutcss');
 
-function updateColor(listCardNode) {
-  // Look at badge text
-  var badgeTextNodes = listCardNode.querySelectorAll('.badge-text');
-  var compoundStates = Array.from(badgeTextNodes).map(function(badgeTextNode) {
-    return getStateFromBadgeText(badgeTextNode);
-  });
+  var configObj = {
+    layout: '',
+    coloring: ''
+  };
+  var hasSpecificConfig = false;
+  var defaultSettingKey = '*';
 
-  // Look at title
-  var titleState = getStateFromTitle(listCardNode.querySelector('.list-card-title'));
-  if (titleState !== undefined)
-    compoundStates.push(titleState);
-
-  // Resolve according to priority
-  var mainState = getMainState(compoundStates);
-  setState(listCardNode, mainState);
-
-  // Set additional class if having a short or past deadline
-  var iconClockNode = listCardNode.querySelector('.badge.is-due-soon') || listCardNode.querySelector('.badge.is-due-now') || listCardNode.querySelector('.badge.is-due-past');
-  if (listCardNode.classList)
-    listCardNode.classList.toggle('urgent-tdl', iconClockNode !== null && (mainState !== 'canceled-tdl'));
-}
-
-function updateSwimlanes(listWrapperNode) {
-  var titleEl = listWrapperNode.querySelector('.list-header-name');
-  if (!titleEl)
-    return;
-  var wantWrap = false;
-  var swimlaneTitle;
-  if (titleEl.value) {
-    var matches = titleEl.value.match(/^.*[\|🏊](.*)/);
-    if (matches && matches.length > 1) {
-      wantWrap = true;
-      swimlaneTitle = matches[1];
+  function onSettingChanged(key, newConfigObj) {
+    if (key === boardId) {
+      hasSpecificConfig = true;
+      //console.log("Tredolist: loading page-specific settings for "+key);
+      //console.log(newConfigObj);
     }
-  }
-  if (listWrapperNode.classList)
-    listWrapperNode.classList.toggle('wrapBefore-tdl', wantWrap);
-  var lineBreakEl = listWrapperNode.previousElementSibling;
-  var hasLineBreakBefore = (lineBreakEl && lineBreakEl.classList && lineBreakEl.classList.contains('line-break-tdl'));
-  if (wantWrap && !hasLineBreakBefore) {
-    // Add
-    var lineBreak = document.createElement('div');
-    lineBreak.className = 'line-break-tdl';
-    lineBreak.setAttribute('data-swimlane-title-tdl', swimlaneTitle.trim());
-    listWrapperNode.parentNode.insertBefore(lineBreak, listWrapperNode);
-  }
-  else if (!wantWrap && hasLineBreakBefore) {
-    // Remove
-    listWrapperNode.parentElement.removeChild(lineBreakEl);
-  }
-  else if (wantWrap && hasLineBreakBefore) {
-    // Modify
-    lineBreakEl.setAttribute('data-swimlane-title-tdl', swimlaneTitle);
-  }
-}
-
-catchNodeByClass('list-card', updateColor);
-
-catchNodeByClass('list-wrapper', updateSwimlanes);
-
-// Make sure we catch late asynchronous badge changes
-var classesToObserve = [
-{className: 'badge', type: 'c', callback: function(node) {
-  var listCardNode = getParent(node, '.list-card');
-  if (listCardNode)
-    updateColor(listCardNode);
-}},
-{className: 'list-header-name-assist', type: 'v', callback: function(node) {
-  var listWrapperNode = getParent(node, '.list-wrapper');
-  if (listWrapperNode)
-    updateSwimlanes(listWrapperNode);
-}}];
-
-
-var rootNode = document.getElementsByClassName('board-canvas');
-if (rootNode) {
-  rootNode = rootNode[0];
-  var callback = function(mutations) {
-    for(var mutation of mutations) {
-      switch (mutation.type) {
-        case 'childList':
-          mutation.addedNodes.forEach(function(node, iNode, nodes) {
-            classesToObserve.forEach(function(obj) {
-              switch (obj.type) {
-                case 'c':
-                  if (node.classList && node.classList.contains(obj.className))
-                    obj.callback(node);
-                  break;
-                case 'v':
-                  if (node.nodeValue !== undefined && node.parentElement && node.parentElement.classList && node.parentElement.classList.contains(obj.className))
-                    obj.callback(node);
-                  break;
-              }
-            });
-          });
+    else if ((key !== defaultSettingKey) || hasSpecificConfig)
+      return;
+    //else
+    //  console.log("Tredolist: loading default settings");
+    
+    for (var configKey in configObj) {
+      if (newConfigObj[configKey] !== configObj[configKey]) {
+        switch (configKey) {
+          case 'layout':
+            setLayout(newConfigObj[configKey]);
+            break;
+          case 'coloring':
+            setNewStylesheet(
+              'theme-'+configObj[configKey]+'.css',
+              'theme-'+newConfigObj[configKey]+'.css');
           break;
-        case 'attributes':
-          if (obj.type === 'a' && mutation.target.classList.contains(obj.className))
-            obj.callback(mutation.target);
-          break;
+        }
+        configObj[configKey] = newConfigObj[configKey];
       }
     }
-  };
-  var observer = new MutationObserver(callback);
-  observer.observe(rootNode, {childList: true, subtree: true});
+  }
+
+  // Load settings
+  chrome.storage.sync.get([boardId], function(result) {
+    if (result[boardId])
+      onSettingChanged(boardId, result[boardId]);
+    else {
+      chrome.storage.sync.get([defaultSettingKey], function(resultD) {
+        if (resultD[defaultSettingKey])
+          onSettingChanged((defaultSettingKey), resultD[defaultSettingKey]);
+      });
+    }
+  });
+  chrome.storage.onChanged.addListener(function(changes, areaName) {
+    for (key in changes) {
+      onSettingChanged(key, changes[key].newValue);
+    }
+  });
+
+  var States = ['canceled-tdl', 'done-tdl', 'for-later-tdl', 'waiting-for-tdl', 'in-progress-tdl']; // in order of priority
+  var Fields = ['Canceled', 'Done', 'For Later', 'Waiting For', 'Started'];
+
+  function getStateFromBadgeText(node) {
+    if (!node)
+      return undefined;
+
+    // Using custom fields
+    for (var iState in Fields) {
+      if (node.innerText === Fields[iState])
+        return States[iState];
+    }
+
+    // Using checklist
+    var toks = node.innerText.match(/^(\d+)\/(\d+)$/);
+    if (toks && toks.length > 2) {
+      var numDone = toks[1];
+      var numTodo = toks[2];
+      if (numDone === numTodo)
+        return 'done-tdl';
+      else if (numDone > 0)
+        return 'in-progress-tdl';
+      else
+        return 'not-started-tdl';
+    }
+    return undefined;
+  }
+
+  function getStateFromTitle(node) {
+    if (node && node.innerText) {
+      if (node.innerText.match(/^\(.*\)$/) !== null)
+        return 'for-later-tdl';
+      if (node.innerText.match(/\<.*\>/) !== null)
+        return 'waiting-for-tdl';
+    }
+    return undefined;
+  }
+
+  function getMainState(compoundStates) {
+    for (var st of States) {
+      if (compoundStates.indexOf(st) !== -1) {
+        return st;
+      }
+    }
+    return undefined;
+  }
+
+  function setState(card, state) {
+    if (!card.classList)
+      return;
+    for (var st of States) {
+      card.classList.toggle(st, (st === state));
+    }
+  }
+
+  function updateColor(listCardNode) {
+    // Look at badge text
+    var badgeTextNodes = listCardNode.querySelectorAll('.badge-text');
+    var compoundStates = Array.from(badgeTextNodes).map(function(badgeTextNode) {
+      return getStateFromBadgeText(badgeTextNode);
+    });
+
+    // Look at title
+    var titleState = getStateFromTitle(listCardNode.querySelector('.list-card-title'));
+    if (titleState !== undefined)
+      compoundStates.push(titleState);
+
+    // Resolve according to priority
+    var mainState = getMainState(compoundStates);
+    setState(listCardNode, mainState);
+
+    // Set additional class if having a short or past deadline
+    var iconClockNode = listCardNode.querySelector('.badge.is-due-soon') || listCardNode.querySelector('.badge.is-due-now') || listCardNode.querySelector('.badge.is-due-past');
+    if (listCardNode.classList)
+      listCardNode.classList.toggle('urgent-tdl', iconClockNode !== null && (mainState !== 'canceled-tdl'));
+  }
+
+  function updateSwimlanes(listWrapperNode) {
+    var titleEl = listWrapperNode.querySelector('.list-header-name');
+    if (!titleEl)
+      return;
+    var wantWrap = false;
+    var swimlaneTitle;
+    if (titleEl.value) {
+      var matches = titleEl.value.match(/^.*[\|🏊](.*)/);
+      if (matches && matches.length > 1) {
+        wantWrap = true;
+        swimlaneTitle = matches[1];
+      }
+    }
+    if (listWrapperNode.classList)
+      listWrapperNode.classList.toggle('wrapBefore-tdl', wantWrap);
+    var lineBreakEl = listWrapperNode.previousElementSibling;
+    var hasLineBreakBefore = (lineBreakEl && lineBreakEl.classList && lineBreakEl.classList.contains('line-break-tdl'));
+    if (wantWrap && !hasLineBreakBefore) {
+      // Add
+      var lineBreak = document.createElement('div');
+      lineBreak.className = 'line-break-tdl';
+      lineBreak.setAttribute('data-swimlane-title-tdl', swimlaneTitle.trim());
+      listWrapperNode.parentNode.insertBefore(lineBreak, listWrapperNode);
+    }
+    else if (!wantWrap && hasLineBreakBefore) {
+      // Remove
+      listWrapperNode.parentElement.removeChild(lineBreakEl);
+    }
+    else if (wantWrap && hasLineBreakBefore) {
+      // Modify
+      lineBreakEl.setAttribute('data-swimlane-title-tdl', swimlaneTitle);
+    }
+  }
+
+  catchNodeByClass('list-card', updateColor);
+
+  catchNodeByClass('list-wrapper', updateSwimlanes);
+
+  // Make sure we catch late asynchronous badge changes
+  var classesToObserve = [
+  {className: 'badge', type: 'c', callback: function(node) {
+    var listCardNode = getParent(node, '.list-card');
+    if (listCardNode)
+      updateColor(listCardNode);
+  }},
+  {className: 'list-header-name-assist', type: 'v', callback: function(node) {
+    var listWrapperNode = getParent(node, '.list-wrapper');
+    if (listWrapperNode)
+      updateSwimlanes(listWrapperNode);
+  }}];
+
+
+  var rootNode = document.getElementsByClassName('board-canvas');
+  if (rootNode) {
+    rootNode = rootNode[0];
+    var callback = function(mutations) {
+      for(var mutation of mutations) {
+        switch (mutation.type) {
+          case 'childList':
+            mutation.addedNodes.forEach(function(node, iNode, nodes) {
+              classesToObserve.forEach(function(obj) {
+                switch (obj.type) {
+                  case 'c':
+                    if (node.classList && node.classList.contains(obj.className))
+                      obj.callback(node);
+                    break;
+                  case 'v':
+                    if (node.nodeValue !== undefined && node.parentElement && node.parentElement.classList && node.parentElement.classList.contains(obj.className))
+                      obj.callback(node);
+                    break;
+                }
+              });
+            });
+            break;
+          case 'attributes':
+            if (obj.type === 'a' && mutation.target.classList.contains(obj.className))
+              obj.callback(mutation.target);
+            break;
+        }
+      }
+    };
+    var observer = new MutationObserver(callback);
+    observer.observe(rootNode, {childList: true, subtree: true});
+  }
 }
